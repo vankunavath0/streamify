bc8d6d358c9da32af4b7a1d5b4119cd487cb1237
vankunavth0 <vankunavthudaykumar@gmail.com>
2025-07-26 22:18:44 +0530

    made changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? ":https://streamify-5tqj.onrender.com/api" : "/api";
+const BASE_URL =
+  import.meta.env.VITE_API_BASE_URL ;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
